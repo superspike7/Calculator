6771642f62a0b5d6427934f900d4dbab0c4263a6
Spike Vinz Cruz <spikevinz@gmail.com>
2020-08-09 09:49:42 +0800

    UI added

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
+const numbersBtn = document.querySelectorAll('.number');
+const input = document.querySelector('.input');
+const clearBtn = document.querySelector('#clear')
+
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
-const operate = (operator, a, b) => {
-    return operator(a, b);
+const operate = (operator) => {
+    if (operator === '+') {
+        return add(a,b);
+    } else if (operator === '-') {
+        return subtract(a,b)
+    }
 }
+// make calculator work
+
+const clear = e => input.innerHTML = "";
+
+const addNum = e => {
+    const numVal = e.target.getAttribute('value');
+    const num = document.createTextNode(numVal)
+    input.appendChild(num);
+}
+
+clearBtn.addEventListener('click', clear);
+numbersBtn.forEach(function(numBtn){
+    numBtn.addEventListener('click', addNum);
+})
