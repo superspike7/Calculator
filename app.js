59e399ef7b4ab7420aca2c85a0a5842baf7765aa
Spike Vinz Cruz <spikevinz@gmail.com>
2020-08-11 07:33:49 +0800

    calculator is now functional

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-const numbersBtn = document.querySelectorAll('.number');
+const numberBtns = document.querySelectorAll('.number');
 const input = document.querySelector('.input');
 const clearBtn = document.querySelector('#clear')
 const backspaceBtn = document.querySelector('#backspace');
 const dotBtn = document.querySelector('.dot');
 const output = document.querySelector('.output');
-const addBtn = document.querySelector('#add');
+const operatorBtns = document.querySelectorAll('.operator');
 const equalsBtn = document.querySelector('#equals');
+const sign = document.querySelector('.sign');
 
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
@@ -14,22 +15,51 @@ const divide = (a, b) => a / b;
 
 
 // make calculator work
-const addNumbers = e => {
+// const addOperation = e => {
+//     const a = input.textContent;
+//     sign.textContent = '+';
+//     if (input.textContent !== null) {
+//         input.innerHTML = ""
+//         output.textContent += a;
+//     }
+// }
+
+const addOperation = e => {
     const a = input.textContent;
-    input.innerHTML = ""
-    output.textContent += a;
+    const operator = e.target.textContent;
+    sign.textContent = operator;
+        if (input.textContent !== null) {
+        input.innerHTML = ""
+        output.textContent += a;
+    } 
 }
+// fix addoparation function to operate without using equals button
 
 const operate = e => {
     const a = output.textContent;
     const b = input.textContent;
-    output.innerHTML = +a + +b;
-    input.innerHTML = ""
+    if (sign.textContent === '+') {
+        clear();
+        output.textContent = add(+a, +b);
+    } else if (sign.textContent === '-') {
+        clear();
+        output.textContent = subtract(+a, +b);
+    } else if (sign.textContent === 'x') {
+        clear();
+        output.textContent = multiply(+a, +b);
+    } else if (sign.textContent === '÷') {
+        clear();
+        output.textContent = divide(+a, +b);
+    }
 }
+
 
 const clear = e => {
     input.innerHTML = ""
-    output.innerHTML = ""};
+    output.innerHTML = ""
+    sign.innerHTML = ""
+
+};
 const backspace = e => {
     if (input.innerHTML === '') {
         console.log('empty')
@@ -52,8 +82,10 @@ const addDot = e => {
 clearBtn.addEventListener('click', clear);
 backspaceBtn.addEventListener('click', backspace);
 dotBtn.addEventListener('click', addDot);
-numbersBtn.forEach(function(numBtn){
+numberBtns.forEach(numBtn => {
     numBtn.addEventListener('click', addNum);
-})
-addBtn.addEventListener('click', addNumbers);
+});
+operatorBtns.forEach(operatorBtn => {
+    operatorBtn.addEventListener('click', addOperation)
+});
 equalsBtn.addEventListener('click', operate);
